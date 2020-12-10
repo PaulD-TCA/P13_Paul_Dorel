927bcff0795d41c515eadce953ab7f8c6dbb4189
Dorel <dorel.paul@gmail.com>
2020-12-10 16:32:56 +0100

    test signup url

--- user/test/test_urls.py
+++ user/test/test_urls.py
@@ -8,9 +8,9 @@ class TestUrls(SimpleTestCase):
         url = reverse('login')
         self.assertEquals(resolve(url).func, user_login)
 
-    def test_user_logout_presentation_url_resolved(self):
-        url = reverse('logout')
-        self.assertEquals(resolve(url).func, user_logout)
+    # def test_user_logout_presentation_url_resolved(self):
+    #     url = reverse('logout')
+    #     self.assertEquals(resolve(url).func, user_logout)
 
     # def test_user_signup_url_resolved(self):
     #     url = reverse('signup')
